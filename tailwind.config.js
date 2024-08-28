f57052a274f3993f5584dbd4d0b2b80693c2433a
maheshbasnet089 <basnetmanish089@gmail.com>
2024-08-28 23:16:29 +0545

    part2

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
     extend: {
       fontFamily : {
         domine : ['Domine','serif']
+      }, 
+      animation : {
+        'rotating'  : 'rotating 5s linear infinite'
+      }, 
+      keyframes : {
+        rotating : {
+          '0%' : {transform : 'rotate(0.0deg)'}, 
+          '25%' : {transform : 'rotate(90deg)'},
+          '50%' : {transform : 'rotate(180deg)'},
+          '75%' : {transform : 'rotate(270deg)'},
+          '100%' : {transform : 'rotate(3600deg)'},
+          
+        }
       }
     },
   },
